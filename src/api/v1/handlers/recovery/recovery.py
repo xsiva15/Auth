e175f7017f0f0963afcc857939696fe7ab25a7c7
boyfws <minkin.d.d@gmail.com>
2025-02-14 04:21:50 +0300

    Закончил описание api

--- src/api/v1/handlers/recovery/recovery.py
+++ src/api/v1/handlers/recovery/recovery.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette import status
 from starlette.responses import RedirectResponse
-
+from src.models import (
+    EmailData,
+    DataForSendingEmail,
+    NoEmailInDataBase,
+    convert_data_to_DataForReset,
+    ResetExpired
+    )
+from src.service import get_recovery_service
 
 
 recovery_router = APIRouter(
@@ -14,21 +21,39 @@ recovery_router = APIRouter(
     "/new_password",
     status_code=status.HTTP_204_NO_CONTENT,
     response_class=RedirectResponse,
-    summary="Используется для изменения текущего пароля пользователя, вся необходимая информация о пользователе передается через токен"
+    summary="Используется для изменения текущего пароля пользователя, вся необходимая информация о пользователе передается через токен",
+    responses={
+        400: {"model": ResetExpired, "detail": "A link for recover was expired"}
+    }
 )
 async def set_new_password(
         token: str,
-        password: str
+        password: str,
+        service=Depends(get_recovery_service)
 ):
-    pass
+    return await service.password_recover(
+        convert_data_to_DataForReset(
+            token=token,
+            new_password=password
+        )
+    )
+
 
 
 @recovery_router.post(
     "/send_email_for_new_password",
     status_code=status.HTTP_204_NO_CONTENT,
-    summary="Используется для отправки пользователю письма с ссылкой на страницу для сброса пароля"
+    summary="Используется для отправки пользователю письма с ссылкой на страницу для сброса пароля",
+    responses={
+        404: {"model": NoEmailInDataBase, "detail": "No such user"}
+    }
 )
 async def send_email_for_recov(
-        email: str
+        email_data: EmailData,
+        service=Depends(get_recovery_service)
 ):
-    pass
+    return await service.send_email_for_recov(
+        DataForSendingEmail(
+            email=email_data.email
+        )
+    )
