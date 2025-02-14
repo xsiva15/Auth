384578c389d0359baf0b6665662193c59c754cac
boyfws <minkin.d.d@gmail.com>
2025-02-14 04:22:22 +0300

    Доавил модель в responses

---
+++ src/models/responses/recovery.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from .EmailNormaliztion import EmailNormalizer
+from starlette import status
+
+
+class EmailData(EmailNormalizer):
+    email: str
+
+
+class DataForSendingEmail(BaseModel):
+    email: str
+
+
+class ResetExpired(BaseModel):
+    status_code: int = status.HTTP_400_BAD_REQUEST,
+    detail: str = "A link for recover was expired"
+
+
+class NoEmailInDataBase(BaseModel):
+    status_code: int = status.HTTP_404_NOT_FOUND,
+    detail: str = "User not found in database"
